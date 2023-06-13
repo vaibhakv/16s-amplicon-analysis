cfb647cf2a4f96b79b1cc50abeb2fc3887b86b59
vaibhakv <vaibhav19191255@gmail.com>
2023-06-13 23:46:20 +0530

    Modified the get_mothur.sh script, closes #9

--- code/get_mothur.sh
+++ code/get_mothur.sh
@@ -3,9 +3,8 @@
 # author: vaibhakv
 # inputs: none
 # outputs: mothur software
-#
 # This is version 1.48.0 for ubuntu distro. 
 # You can download the specific version for your distro from the link provided in the README file
 
-wget -nc -P code/mothur/ https://github.com/mothur/mothur/releases/download/v1.48.0/Mothur.Ubuntu_20.zip
-unzip -n -d code/mothur/ code/mothur/Mothur.Ubuntu_20.zip
+wget -nc -P code/ https://github.com/mothur/mothur/releases/download/v1.48.0/Mothur.Ubuntu_20.zip
+unzip -n -d code/ code/Mothur.Ubuntu_20.zip
