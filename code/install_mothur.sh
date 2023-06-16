2b3a7059e607d74f92a635dc676f6deb5b651e47
vaibhakv <vaibhav19191255@gmail.com>
2023-06-16 10:36:23 +0530

    install_mother.sh modified

--- code/install_mothur.sh
+++ code/install_mothur.sh
@@ -9,3 +9,9 @@
 wget -nc -P code/ https://github.com/mothur/mothur/releases/download/v1.48.0/Mothur.Ubuntu_20.zip
 unzip -n -d code/ code/Mothur.Ubuntu_20.zip
 
+if [[ $? -eq 0 ]]
+then
+	touch "code/mothur/mothur" 
+else 
+	echo "FAIL: were not able to successfully install mothur"
+fi
